015b55b82e4e2150120f0e84484788f098358378
Dennis Villegas <129340417+Dennisvc12@users.noreply.github.com>
2025-11-20 13:57:31 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10215499
+    "domain": "3nbf4.com",
+    "zoneId": 10215539
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
